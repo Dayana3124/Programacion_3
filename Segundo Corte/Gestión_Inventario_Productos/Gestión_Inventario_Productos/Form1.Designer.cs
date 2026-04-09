82f7ce8a2dc4c579b1b05a49faa8f323798bea81
Dayana1124 <karoldvargas@unicesar.edu.co>
2026-04-09 15:11:30 -0500

    Gestión de Inventario de Productos

---
+++ Segundo Corte/Gestión_Inventario_Productos/Gestión_Inventario_Productos/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Gestión_Inventario_Productos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numStockInicial = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numStockMinimo = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbReducido = new System.Windows.Forms.RadioButton();
+            this.rbGeneral = new System.Windows.Forms.RadioButton();
+            this.rbExento = new System.Windows.Forms.RadioButton();
+            this.chkEsPerecedero = new System.Windows.Forms.CheckBox();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockInicial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockMinimo)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestión de Inventario de Productos";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(97, 124);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(128, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(180, 89);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(260, 20);
+            this.txtNombreProducto.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOMBRE DEL PRODUCTO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CODIGO:";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Electrónica",
+            "Alimentos",
+            "Ropa"});
+            this.cmbCategoria.Location = new System.Drawing.Point(97, 149);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(128, 21);
+            this.cmbCategoria.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CATEGORIA:";
+            // 
+            // numStockInicial
+            // 
+            this.numStockInicial.Location = new System.Drawing.Point(404, 124);
+            this.numStockInicial.Name = "numStockInicial";
+            this.numStockInicial.Size = new System.Drawing.Size(120, 20);
+            this.numStockInicial.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label5.Location = new System.Drawing.Point(299, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "STOCK INICIAL:";
+            // 
+            // numStockMinimo
+            // 
+            this.numStockMinimo.Location = new System.Drawing.Point(404, 155);
+            this.numStockMinimo.Name = "numStockMinimo";
+            this.numStockMinimo.Size = new System.Drawing.Size(120, 20);
+            this.numStockMinimo.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label6.Location = new System.Drawing.Point(299, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "STOCK MINIMO:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbReducido);
+            this.groupBox1.Controls.Add(this.rbGeneral);
+            this.groupBox1.Controls.Add(this.rbExento);
+            this.groupBox1.Location = new System.Drawing.Point(18, 230);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IVA";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rbReducido
+            // 
+            this.rbReducido.AutoSize = true;
+            this.rbReducido.Location = new System.Drawing.Point(6, 65);
+            this.rbReducido.Name = "rbReducido";
+            this.rbReducido.Size = new System.Drawing.Size(88, 17);
+            this.rbReducido.TabIndex = 2;
+            this.rbReducido.TabStop = true;
+            this.rbReducido.Text = "Reducido 5%";
+            this.rbReducido.UseVisualStyleBackColor = true;
+            // 
+            // rbGeneral
+            // 
+            this.rbGeneral.AutoSize = true;
+            this.rbGeneral.Location = new System.Drawing.Point(6, 42);
+            this.rbGeneral.Name = "rbGeneral";
+            this.rbGeneral.Size = new System.Drawing.Size(85, 17);
+            this.rbGeneral.TabIndex = 1;
+            this.rbGeneral.TabStop = true;
+            this.rbGeneral.Text = "General 19%";
+            this.rbGeneral.UseVisualStyleBackColor = true;
+            // 
+            // rbExento
+            // 
+            this.rbExento.AutoSize = true;
+            this.rbExento.Location = new System.Drawing.Point(6, 19);
+            this.rbExento.Name = "rbExento";
+            this.rbExento.Size = new System.Drawing.Size(75, 17);
+            this.rbExento.TabIndex = 0;
+            this.rbExento.TabStop = true;
+            this.rbExento.Text = "Exento 0%";
+            this.rbExento.UseVisualStyleBackColor = true;
+            // 
+            // chkEsPerecedero
+            // 
+            this.chkEsPerecedero.AutoSize = true;
+            this.chkEsPerecedero.Font = new System.Drawing.Font("Georgia", 9F);
+            this.chkEsPerecedero.Location = new System.Drawing.Point(302, 230);
+            this.chkEsPerecedero.Name = "chkEsPerecedero";
+            this.chkEsPerecedero.Size = new System.Drawing.Size(88, 19);
+            this.chkEsPerecedero.TabIndex = 12;
+            this.chkEsPerecedero.Text = "Perecedero";
+            this.chkEsPerecedero.UseVisualStyleBackColor = true;
+            this.chkEsPerecedero.CheckedChanged += new System.EventHandler(this.chkEsPerecedero_CheckedChanged);
+            // 
+            // dtpFechaVencimiento
+            // 
+            this.dtpFechaVencimiento.Enabled = false;
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(470, 269);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaVencimiento.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Georgia", 9F);
+            this.label7.Location = new System.Drawing.Point(299, 272);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(165, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "FECHA DE VENCIMIENTO:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnLimpiar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(612, 370);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(122, 68);
+            this.btnLimpiar.TabIndex = 15;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Peru;
+            this.btnGuardar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnGuardar.Location = new System.Drawing.Point(24, 370);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(122, 68);
+            this.btnGuardar.TabIndex = 17;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtpFechaVencimiento);
+            this.Controls.Add(this.chkEsPerecedero);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numStockMinimo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numStockInicial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numStockInicial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockMinimo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numStockInicial;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numStockMinimo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbExento;
+        private System.Windows.Forms.RadioButton rbReducido;
+        private System.Windows.Forms.RadioButton rbGeneral;
+        private System.Windows.Forms.CheckBox chkEsPerecedero;
+        private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ Segundo Corte/Gestión_Inventario_Productos/Gestión_Inventario_Productos/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestión_Inventario_Productos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkEsPerecedero_CheckedChanged(object sender, EventArgs e)
+        {
+            dtpFechaVencimiento.Enabled = chkEsPerecedero.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+
+            if (txtCodigo.Text.Trim() == "" ||
+         txtNombreProducto.Text.Trim() == "" ||
+         cmbCategoria.SelectedIndex == -1)
+            {
+                btnGuardar.BackColor = Color.Red;
+                btnGuardar.ForeColor = Color.White;
+
+                await Task.Delay(1000);
+
+                btnGuardar.BackColor = Color.Peru; 
+                btnGuardar.ForeColor = Color.Black;
+
+                MessageBox.Show("Todos los campos son obligatorios");
+                return;
+            }
+
+            string codigo = txtCodigo.Text.Trim();
+
+            if (codigo.Length != 9 ||
+                codigo.Substring(0, 5).ToUpper() != "PROD-" ||
+                !int.TryParse(codigo.Substring(5, 4), out _))
+            {
+                btnGuardar.BackColor = Color.Red;
+                btnGuardar.ForeColor = Color.White;
+
+                await Task.Delay(1000);
+
+                btnGuardar.BackColor = Color.Peru;
+                btnGuardar.ForeColor = Color.Black;
+
+                MessageBox.Show("El código debe tener el formato PROD-0000");
+                return;
+            }
+
+            if (numStockInicial.Value < numStockMinimo.Value)
+            {
+                btnGuardar.BackColor = Color.Red;
+                btnGuardar.ForeColor = Color.White;
+
+                await Task.Delay(1000);
+
+                btnGuardar.BackColor = Color.Peru;
+                btnGuardar.ForeColor = Color.Black;
+
+                MessageBox.Show("El stock inicial no puede ser menor al stock mínimo");
+                return;
+            }
+
+            if (!rbExento.Checked && !rbGeneral.Checked && !rbReducido.Checked)
+            {
+                btnGuardar.BackColor = Color.Red;
+                btnGuardar.ForeColor = Color.White;
+
+                await Task.Delay(1000);
+
+                btnGuardar.BackColor = Color.Peru;
+                btnGuardar.ForeColor = Color.Black;
+
+                MessageBox.Show("Debe seleccionar un tipo de IVA");
+                return;
+            }
+
+            if (chkEsPerecedero.Checked && dtpFechaVencimiento.Value <= DateTime.Now)
+            {
+                btnGuardar.BackColor = Color.Red;
+                btnGuardar.ForeColor = Color.White;
+
+                await Task.Delay(1000);
+
+                btnGuardar.BackColor = Color.Peru;
+                btnGuardar.ForeColor = Color.Black;
+
+                MessageBox.Show("La fecha de vencimiento debe ser futura");
+                return;
+            }
+
+           
+            btnGuardar.BackColor = Color.Green;
+            btnGuardar.ForeColor = Color.White;
+
+            await Task.Delay(1000);
+
+            btnGuardar.BackColor = Color.Peru;
+            btnGuardar.ForeColor = Color.Black;
+
+            MessageBox.Show("Producto registrado correctamente");
+        
+        }
+
+        private async void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            btnLimpiar.BackColor = Color.Orange;
+            btnLimpiar.ForeColor = Color.White;
+
+            await Task.Delay(1000);
+
+            
+            btnLimpiar.BackColor = Color.PaleGoldenrod;
+            btnLimpiar.ForeColor = Color.Black;
+
+            
+            txtCodigo.Clear();
+            txtNombreProducto.Clear();
+
+            cmbCategoria.SelectedIndex = -1;
+
+            numStockInicial.Value = 0;
+            numStockMinimo.Value = 0;
+
+            rbExento.Checked = false;
+            rbGeneral.Checked = false;
+            rbReducido.Checked = false;
+
+            chkEsPerecedero.Checked = false;
+            dtpFechaVencimiento.Enabled = false;
+
+            MessageBox.Show("Formulario limpio");
+        }
+    }
+    
+    
+    
+}
