8aa2228d083111a9200c946df00a7b307c6c7836
Dayana1124 <karoldvargas@unicesar.edu.co>
2026-04-12 14:17:31 -0500

    Actividad Persistencia

---
+++ Control_Ventas_Diarias/Control_Ventas_Diarias/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Control_Ventas_Diarias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.numMonto = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardarTransaccion = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstHistorial = new System.Windows.Forms.ListBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 46);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(741, 185);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightPink;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Control De Ventas Diarias";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGuardarTransaccion);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numMonto);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Nirmala Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.groupBox1, 2);
+            this.groupBox1.Size = new System.Drawing.Size(735, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nueva Venta";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID Transacción ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(6, 48);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(209, 23);
+            this.txtID.TabIndex = 1;
+            this.txtID.Validating += new System.ComponentModel.CancelEventHandler(this.txtID_Validating);
+            // 
+            // numMonto
+            // 
+            this.numMonto.DecimalPlaces = 2;
+            this.numMonto.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.numMonto.Location = new System.Drawing.Point(388, 48);
+            this.numMonto.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numMonto.Name = "numMonto";
+            this.numMonto.Size = new System.Drawing.Size(189, 23);
+            this.numMonto.TabIndex = 2;
+            this.numMonto.Validating += new System.ComponentModel.CancelEventHandler(this.numMonto_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Monto ($)";
+            // 
+            // btnGuardarTransaccion
+            // 
+            this.btnGuardarTransaccion.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnGuardarTransaccion.Location = new System.Drawing.Point(6, 107);
+            this.btnGuardarTransaccion.Name = "btnGuardarTransaccion";
+            this.btnGuardarTransaccion.Size = new System.Drawing.Size(137, 57);
+            this.btnGuardarTransaccion.TabIndex = 4;
+            this.btnGuardarTransaccion.Text = "Guardar Transacción";
+            this.btnGuardarTransaccion.UseVisualStyleBackColor = false;
+            this.btnGuardarTransaccion.Click += new System.EventHandler(this.btnGuardarTransaccion_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.btnCargar);
+            this.groupBox2.Controls.Add(this.lstHistorial);
+            this.groupBox2.Font = new System.Drawing.Font("Nirmala Text", 9.75F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.Location = new System.Drawing.Point(12, 237);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(741, 211);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Historial";
+            // 
+            // lstHistorial
+            // 
+            this.lstHistorial.Font = new System.Drawing.Font("Verdana", 9.75F);
+            this.lstHistorial.FormattingEnabled = true;
+            this.lstHistorial.ItemHeight = 16;
+            this.lstHistorial.Location = new System.Drawing.Point(9, 31);
+            this.lstHistorial.Name = "lstHistorial";
+            this.lstHistorial.Size = new System.Drawing.Size(707, 132);
+            this.lstHistorial.TabIndex = 0;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnCargar.Location = new System.Drawing.Point(12, 168);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(99, 37);
+            this.btnCargar.TabIndex = 5;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.ForeColor = System.Drawing.Color.Blue;
+            this.lblTotal.Location = new System.Drawing.Point(613, 178);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(87, 17);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "TOTAL: $0.00";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonto)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardarTransaccion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numMonto;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstHistorial;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Control_Ventas_Diarias/Control_Ventas_Diarias/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Ventas_Diarias
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtID_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtID.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtID, "ID obligatorio");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider1.SetError(txtID, "");
+            }
+        }
+
+        private void numMonto_Validating(object sender, CancelEventArgs e)
+        {
+            if (numMonto.Value <= 0)
+            {
+                errorProvider1.SetError(numMonto, "El monto debe ser mayor a 0");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider1.SetError(numMonto, "");
+            }
+        }
+
+        private void btnGuardarTransaccion_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+
+            if (!this.ValidateChildren())
+            {
+                return;
+            }
+
+            string ruta = "ventas.txt";
+
+            string linea = txtID.Text + ", " + numMonto.Value.ToString();
+
+            File.AppendAllText(ruta, linea + Environment.NewLine);
+
+            MessageBox.Show("Transacción guardada");
+
+            txtID.Clear();
+            numMonto.Value = 0;
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            lstHistorial.Items.Clear();
+
+            string ruta = "ventas.txt";
+            double total = 0;
+
+            if (File.Exists(ruta))
+            {
+                string[] lineas = File.ReadAllLines(ruta);
+
+                foreach (string linea in lineas)
+                {
+                    lstHistorial.Items.Add(linea);
+
+                    string[] datos = linea.Split(',');
+
+                    if (datos.Length == 2)
+                    {
+                        double monto;
+                        if (double.TryParse(datos[1].Trim(), out monto))
+                        {
+                            total += monto;
+                        }
+                    }
+                }
+
+                lblTotal.Text = "TOTAL: $" + total.ToString("0.00");
+            }
+            else
+            {
+                MessageBox.Show("No hay ventas registradas");
+            }
+        }
+    }
+}
